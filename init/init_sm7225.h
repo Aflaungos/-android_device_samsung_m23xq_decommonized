81e328c41b37ce51f5867e6f3cbe53fda931de78
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2023-02-12 13:29:06 -0300

    Update init_sm7225.h

--- init/init_sm7225.h
+++ init/init_sm7225.h
@@ -4,13 +4,8 @@
 #include <string.h>
 
 enum device_variant {
-    VARIANT_A426B = 0,
-    VARIANT_A426N,
-    VARIANT_A426U,
     VARIANT_M236B,
     VARIANT_E236B,
-    VARIANT_M426B,
-    VARIANT_MAX
 };
 
 typedef struct {
@@ -19,42 +14,18 @@ typedef struct {
 } variant;
 
 static const variant international_models = {
-    .model = "SM-A426B",
-    .codename = "a42xq"
-};
-
-static const variant international_models_m23 = {
     .model = "SM-M236B",
     .codename = "m23xq"
 };
 
-static const variant india_models_m23 = {
+static const variant india_models = {
     .model = "SM-E236B",
     .codename = "m23xq"
 };
 
-static const variant kor_models = {
-    .model = "SM-A426N",
-    .codename = "a42xq"
-};
-
-static const variant america_models = {
-    .model = "SM-A426U",
-    .codename = "a42xq"
-};
-
-static const variant indea_m_models = {
-    .model = "SM-M426B",
-    .codename = "a42xq"
-};
-
 static const variant *all_variants[VARIANT_MAX] = {
     &international_models,
-    &international_models_m23,
-    &india_models_m23,
-    &kor_models,
-    &america_models,
-    &indea_m_models,
+    &india_models,
 };
 
 #endif // INIT_SEC_H
